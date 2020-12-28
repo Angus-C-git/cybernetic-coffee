c69d8dd0e540ec7ca5d0830b322f6ae0189b4392
zephyr <anguscornall@gmail.com>
2020-12-28 22:06:42 +1100

    Publishing to gh-pages (publish.sh)

--- bundle.min.4a282f1d16d9eca0f747b807bc0884f9edfbe87da708abbf4c0ed74f971c7db09a5a270c329147bd85756bc9b294147006a6b7c1fc5175762e3bf44d923d8316.js
+++
@@ -1,8 +0,0 @@
-function twitterFeed(){console.log("Pulling tweets =>");document.getElementById("feed").innerHTML="";document.getElementById('loader').innerHTML="<span id=\"loader\"><i class=\"fas fa-spinner\"></i> Loading ...</span>";let nonSelected=document.getElementById('github');nonSelected.classList.remove('feed-selected');nonSelected.classList.add('feed-non-selected');let selected=document.getElementById('twitter');selected.classList.remove('feed-non-selected');selected.classList.add('feed-selected');let requestOptions={method:'GET',redirect:'follow'};fetch("https://cybernetic-coffee-api.herokuapp.com/api/tweets",requestOptions).then(response=>response.json()).then(result=>{console.log(result.data);let feed=document.getElementById("feed");feed.innerHTML='';document.getElementById('loader').innerHTML=" ";result.data.map(tweet=>{let icon=(tweet.text.indexOf('RT')>=0)?`<i class="fas fa-retweet"></i>`:`<i class="fab fa-twitter">`;feed.innerHTML+=`<li class="timeline-event">
-                                        <label class="timeline-event-icon"></label>
-                                        <div class="timeline-event-copy">
-                                            <p class="timeline-event-thumbnail">Once Upon A Time ...</p>
-                                            <h3>${icon}</i> by ghostinthefiber</h3>
-                                            <p>${tweet.text}</p>
-                                        </div>
-                                    </li>`});}).catch(error=>console.log('error',error));}
