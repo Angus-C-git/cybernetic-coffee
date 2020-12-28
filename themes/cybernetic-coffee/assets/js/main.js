5eba4b50d4df05e46ed36200d94bef042b7f029a
zephyr <anguscornall@gmail.com>
2020-12-28 22:06:37 +1100

    UPDATE: activity feed, fixes and patches

--- themes/cybernetic-coffee/assets/js/main.js
+++ themes/cybernetic-coffee/assets/js/main.js
@@ -1,9 +1,9 @@
 
 function twitterFeed() {
-    console.log("Pulling tweets =>");
-    document.getElementById("feed").innerHTML = "";
-    document.getElementById('loader').innerHTML = "<span id=\"loader\"><i class=\"fas fa-spinner\"></i> Loading ...</span>";
+    document.getElementById('feed').innerHTML = '';
 
+    const loader = document.getElementById('loader');
+    loader.innerHTML = "<span id=\"loader\"><i class=\"fas fa-spinner\"></i> Loading ...</span>";
 
     let nonSelected = document.getElementById('github');
     nonSelected.classList.remove('feed-selected');
@@ -21,10 +21,10 @@ function twitterFeed() {
     fetch("https://cybernetic-coffee-api.herokuapp.com/api/tweets", requestOptions)
         .then(response => response.json())
         .then(result => {
-            console.log(result.data);
             let feed = document.getElementById("feed");
             feed.innerHTML = '';
-            document.getElementById('loader').innerHTML = " ";
+            loader.innerHTML = '';
+
             result.data.map(tweet => {
                 let icon = (tweet.text.indexOf('RT') >= 0) ? `<i class="fas fa-retweet"></i>` : `<i class="fab fa-twitter">`;
                 feed.innerHTML += `<li class="timeline-event">
